f959280f04c5106091f8cb6064908cf1c6ddfd58
AhmedHossam777 <ahmedhossam9226@gmail.com>
2024-12-20 16:05:26 +0200

    feat[server] : Add request timeout interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './common/pipes/validation.pipe';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalFilters(new GlobalExceptionFilter());
 	app.useGlobalPipes(new ValidationPipe());
+	app.useGlobalInterceptors(new TimeoutInterceptor());
+
 	await app.listen(3000);
 }
+
 bootstrap();
