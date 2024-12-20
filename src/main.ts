fffb3393e929a7d99d0db5c81cd2bd9b0322d359
AhmedHossam777 <ahmedhossam9226@gmail.com>
2024-12-20 15:57:24 +0200

    feat[server] : implement in main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from './common/pipes/validation.pipe';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.useGlobalFilters(new GlobalExceptionFilter());
 	app.useGlobalPipes(new ValidationPipe());
 	await app.listen(3000);
 }
